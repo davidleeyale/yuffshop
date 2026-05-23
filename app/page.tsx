65aaba237adbfb8370c55a82d2fd20ed719180fa
David Lee <david.lee@aya.yale.edu>
2026-05-23 11:39:54 -0400

    Add logo and tailwind css for styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import Image from 'next/image'
+
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+    return <>
+        <div className="flex items-center justify-center min-h-screen">
+            <div className={""}>
+            <Image src="/yuffshop-logo.png" height={800} width={800} alt="Yuffshop Logo"
+                   className="w-full h-full object-cover"></Image>
+            </div>
+        </div>
+    </>
 }
