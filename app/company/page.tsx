62482f04602837a22001dfee93c2826503d404dc
David Lee <david.lee@aya.yale.edu>
2026-05-24 09:59:30 -0400

    Update with more learning information

--- app/company/page.tsx
+++ app/company/page.tsx
@@ -4,5 +4,8 @@ export default function CompanyPage() {
         <p>A company is a group of resources (people, money, technology) that provides service(s) and/or builds product(s).</p>
         <p>It's important to solve a problem that creates "value."</p>
         <p>Value is not only measured with money, but it is one perspective to measure value in society.</p>
+
+        <p>In a competitive market, a company should have a strategy that optimizes the operations of providing service(s) and/or building product(s)
+        to do it more efficiently and/or more effectively to solve the problem(s) that best meets the customers' needs.</p>
     </>;
 }
