eacc30f25f77a6ddd20f77551f6f10e17c37e606
David Lee <david.lee@aya.yale.edu>
2026-05-24 08:37:41 -0400

    Reduce image size to fit in smaller screen

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ export default function Page() {
     return <>
         <div className="flex items-center justify-center min-h-screen">
             <div className={""}>
-            <Image src="/yuffshop-logo.png" height={800} width={800} alt="Yuffshop Logo"
-                   className="w-full h-full object-cover"></Image>
+            <Image src="/yuffshop-logo.png" height={600} width={600} alt="Yuffshop Logo"
+                   loading="eager"
+                   className="object-contain h-full w-full"></Image>
             </div>
         </div>
     </>
