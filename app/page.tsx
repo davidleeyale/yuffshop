de25bbe93a8b6e86d481a567b1eaa59bf1abde7f
David Lee <david.lee@aya.yale.edu>
2026-05-24 10:00:18 -0400

    Updated styling for homepage

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,7 @@ export default function Page() {
                    className=""></Image>
             </div>
             <p>
-                Company branding should build connection with customers in a way that helps remember the company's products and services
-            </p>
-            <p>
+                Company branding should build connection with customers in a way that helps remember the company's products and services.<br /><br />
                 This website is built with <a href="https://nextjs.org/" className="text-blue-500" target="blank">Next.JS</a> and <a href="https://tailwindcss.com" className="text-blue-500" target="blank">Tailwind CSS</a>. The <a href="https://github.com/davidleeyale/yuffshop" className="text-blue-500" target="blank">code repository</a> is on <a href="https://www.github.com" className="text-blue-500" target="blank">GitHub</a> and deploys to <a href="https://www.vercel.com" className="text-blue-500" target="blank">Vercel</a>.
             </p>
         </div>
