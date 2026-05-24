c1b425951bd6a8a7d7c827e7b05226a7d6d8014e
David Lee <david.lee@aya.yale.edu>
2026-05-24 09:23:57 -0400

    Adding technology stack information with links

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,9 @@ export default function Page() {
                    loading="eager"
                    className=""></Image>
             </div>
+            <p>
+                This website is built with <a href="https://nextjs.org/" target="blank">Next.JS</a> and <a href="https://tailwindcss.com" target="blank">Tailwind CSS</a>. The <a href="https://github.com/davidleeyale/yuffshop" target="blank">code repository</a> is on <a href="https://www.github.com" target="blank">GitHub</a> and deploys to <a href="https://www.vercel.com" target="blank">Vercel</a>.
+            </p>
         </div>
     </>
 }
